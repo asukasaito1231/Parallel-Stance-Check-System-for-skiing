ef72b59414ec7293e60f8a9a43e4c55891fef642
asukasaito1231 <asukasaito1231@gmail.com>
2025-06-27 14:26:01 +0900

    画像は3次元表示成功

--- blazePose.py
+++ blazePose.py
@@ -2,8 +2,6 @@ import cv2
 import mediapipe as mp
 import matplotlib.pyplot as plt
 import numpy as np
-import io
-
 # 検出結果の描画
 def plot_world_landmarks(
     plt,
@@ -83,91 +81,33 @@ def plot_world_landmarks(
     ax.plot(left_body_side_x, left_body_side_y, left_body_side_z)
     ax.plot(shoulder_x, shoulder_y, shoulder_z)
     ax.plot(waist_x, waist_y, waist_z)
-
 def main():
-    # 入力動画ファイルと出力動画ファイル
-    input_video_path = 'btest.mp4'
-    output_video_path = 'blaze_pose_output.mp4'
-
     BaseOptions = mp.tasks.BaseOptions
     PoseLandmarker = mp.tasks.vision.PoseLandmarker
     PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
     VisionRunningMode = mp.tasks.vision.RunningMode
     options = PoseLandmarkerOptions(
         base_options=BaseOptions(model_asset_path='pose_landmarker_full.task'),
-        running_mode=VisionRunningMode.VIDEO,
+        running_mode=VisionRunningMode.IMAGE,
         num_poses=1)
-
     with PoseLandmarker.create_from_options(options) as landmarker:
-        cap = cv2.VideoCapture(input_video_path)
-        if not cap.isOpened():
-            print(f"エラー: 動画ファイル {input_video_path} を開けませんでした。")
-            return
-
-        # 3Dプロットの準備と出力動画の解像度取得
+        # 画像ファイルの読み込み
+        mp_image = mp.Image.create_from_file('image.png')
+        # 解析実行
+        pose_landmarker_result = landmarker.detect(mp_image)
+        
+        # 解析結果を描画
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
         fig.subplots_adjust(left=0.0, right=1, bottom=0, top=1)
-        
-        # ダミープロットで出力画像のサイズを取得
-        buf = io.BytesIO()
-        plt.savefig(buf, format='png')
-        buf.seek(0)
-        img_arr = np.frombuffer(buf.getvalue(), dtype=np.uint8)
-        buf.close()
-        plot_img = cv2.imdecode(img_arr, 1)
-        height, width, _ = plot_img.shape
-        
-        fps = cap.get(cv2.CAP_PROP_FPS)
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
-
-        timestamp_ms = 0
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
-
-            # rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-
-            timestamp_ms = int(cap.get(cv2.CAP_PROP_POS_MSEC))
-            pose_landmarker_result = landmarker.detect_for_video(mp_image, timestamp_ms)
-
-            if pose_landmarker_result.pose_world_landmarks:
-                for marks in pose_landmarker_result.pose_world_landmarks:
-                    plot_world_landmarks(
-                        plt,
-                        ax,
-                        marks,
-                    )
-
-                buf = io.BytesIO()
-                plt.savefig(buf, format='png')
-                buf.seek(0)
-                img_arr = np.frombuffer(buf.getvalue(), dtype=np.uint8)
-                buf.close()
-                plot_img = cv2.imdecode(img_arr, 1)
-                
-                # リアルタイム表示
-                cv2.imshow('BlazePose 3D Visualization', plot_img)
-                
-                # out.write(plot_img)
-            else:
-                # ポーズが検出されない場合は黒い画面を表示
-                black_frame = np.zeros((height, width, 3), dtype=np.uint8)
-                cv2.imshow('BlazePose 3D Visualization', black_frame)
-                out.write(black_frame)
-
-            # 'q'キーまたはウィンドウの×ボタンで終了
-            if cv2.waitKey(1) & 0xFF == ord('q') or cv2.getWindowProperty('BlazePose 3D Visualization', cv2.WND_PROP_VISIBLE) < 1:
-                break
-
-        cap.release()
-        out.release()
-        cv2.destroyAllWindows()
-        plt.close(fig)
-        print(f"動画を {output_video_path} に保存しました。")
-
+        for marks in pose_landmarker_result.pose_world_landmarks:
+            plot_world_landmarks(
+                plt,
+                ax,
+                marks,
+            )
+        plt.savefig('result.jpg')
+        plt.show()
+    
 if __name__ == '__main__':
     main()
