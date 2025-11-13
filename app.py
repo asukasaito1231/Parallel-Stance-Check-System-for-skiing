1e20be78e1b5cfd7f46694a60308b21dd9494195
asukasaito1231 <asukasaito1231@gmail.com>
2025-11-13 12:10:55 +0900

    撮影方法に応じたラベル・フレーム表示

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
 import os
+import sys
+sys.path.append(r"C:\Users\asuka\thesis\backGround")
+#from yoloReversed import main
+#from yolo import main
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
-# ダミー解析関数
-def check_parallel(video_path):
-    # 本当はここでOpenCVとかで解析する
-    # いまはダミーで「崩れたフレーム数」を返す
-    return 5  # 仮に5枚あったことにする
-
 @app.route("/", methods=["GET", "POST"])
 def index():
 
-    error_msg = ""  # エラーメッセージ用
+    error_msg = ""
+    ps_result_video_name=""
 
     if request.method == "POST":
 
         # ファイルを受け取る
         file = request.files.get("video")
+        
+        # 撮影方法を受け取る
+        shooting_method = request.form.get("shooting_method")
 
         # ファイルが選択されていない場合
         if not file or file.filename == "":
@@ -29,29 +31,33 @@ def index():
         elif not file.mimetype.startswith("video/"):
             error_msg = "動画ファイルのみアップロード可能です"
 
+        elif not shooting_method:
+            error_msg = "撮影方法が選択されてません"
+
         else:
             # 保存先パス
             filepath = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
 
             file.save(filepath)  # 今は保留
 
-            # ダミー解析呼び出し
-            error_frames = check_parallel(filepath)
+            '''
+            if shooting_method == "後ろから撮影":
+                ps_result_video_name=yolo.main(initial_position)
 
+            else:
+                ps_result_video_name=yoloReversed.main(initial_position)
+            '''
             # /result にリダイレクト
-            return redirect(url_for("result", filename=file.filename, errors=error_frames))
+            return redirect(url_for("result", ps_result_video_name=ps_result_video_name.filename))
 
     return render_template("index.html", error=error_msg)
 
 @app.route("/result")
 def result():
 
-    filename = request.args.get("filename")
+    ps_result_video_name = request.args.get("ps_result_video_name")
 
-    errors = request.args.get("errors", 0)
-
-    return render_template("result.html", filename=filename, errors=errors)
-
+    return render_template("result.html", ps_result_video_name=ps_result_video_name.filename)
 
 if __name__ == "__main__":
 
